ab7129532aa9ff3683b6580a6256e085d19cdb6e
CarlaTincu <carla.tincu@lateralgroup.com>
2023-07-30 17:34:23 +0300

    Add project files.

---
+++ TryingDapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Data.SqlTypes;
+using System.Data.Common;
+using TryingDapper;
+using System.Data.SqlClient;
+using Dapper;
+
+public class Program
+{
+    private static readonly string connectionString = "Data Source=CARLA\\SQLEXPRESS;Initial Catalog=Employees;Trusted_Connection=True;TrustServerCertificate=True;";
+    public static void Main()
+    {
+        List<Employee> employees = GetAllEmployees();
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"ID : {employee.Id}, Name : {employee.LastName}");
+        }
+    }
+    public static List<Employee> GetAllEmployees()
+    {
+        string query = "SELECT id, FirstName, LastName FROM Employees";
+        using(var conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+            List<Employee> employees = conn.Query<Employee>(query).ToList();
+            return employees;
+        }
+    }
+}
